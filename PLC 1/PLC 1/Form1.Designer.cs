e3ea0711f2f87eab66de0f081fe75e99b0debd36
albadmr <82682556+albadmr@users.noreply.github.com>
2022-07-28 12:39:14 -0700

    Add files via upload

---
+++ PLC 1/PLC 1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace PLC_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(367, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "AUTORIZAR CARGA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(28, 105);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(129, 114);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(237, 105);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(129, 114);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(450, 105);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(129, 114);
+            this.panel3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(234, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(447, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(220, 319);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(275, 263);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "label8";
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(450, 250);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(129, 114);
+            this.panel4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 450);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Control de bahìa";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ PLC 1/PLC 1/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Modbus.Device;
+using System.Net.Sockets;
+using System.IO.IsolatedStorage;
+using System.IO;
+
+
+namespace PLC_1
+{
+    public partial class Form1 : Form
+    {
+        
+        ModbusIpMaster maestroPLC;
+        bool[] coils = null;
+        ushort[] holdings=null;
+        bool despachando = false;
+        string inicioDeTransaccion;
+        string finDeTransaccion=null;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            
+            EstadoPLC();
+            timer1.Enabled = true;
+            inicio();
+            
+
+        }
+
+        //void GuardarDatos()
+        //{
+        //    IsolatedStorageFile isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
+
+        //    if (isoStore.FileExists("TestStore.txt"))
+        //    {
+        //        programaCerrado = true;
+        //    }
+        //    else
+        //    {
+        //        using (IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream("TestStore.txt", FileMode.CreateNew, isoStore))
+        //        {
+        //            using (StreamWriter writer = new StreamWriter(isoStream))
+        //            {
+        //                writer.WriteLine("Hello Isolated Storage");
+        //                Console.WriteLine("You have written to the file.");
+        //            }
+        //            programaCerrado = false;
+        //        }
+        //        //programaCerrado = false;
+        //    }
+        //    //if (s)
+
+        //}
+
+        public async void EstadoPLC()
+        {
+            
+                try
+                {
+                    TcpClient clientePLC = new TcpClient();
+
+                    await clientePLC.ConnectAsync("192.168.1.10", 502);
+
+                    maestroPLC = ModbusIpMaster.CreateIp(clientePLC);
+
+                    label2.Text = "PLC conectado";
+                    
+            }
+
+                catch (Exception ex)
+                {
+                    label2.Text = "Error en la conexion";
+                }
+              
+            
+            
+            
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var holdings = await maestroPLC.ReadHoldingRegistersAsync(1, 0, 8);
+                var tag = holdings[0];
+                label7.Text = tag.ToString();
+                maestroPLC.WriteMultipleCoils(5, new bool[] { true });
+            }
+            catch
+            {
+                EstadoPLC();
+            }
+        }
+
+       
+        private async void timer1_Tick_1(object sender, EventArgs e)
+        {
+            
+
+            if (maestroPLC== null)
+            {
+                return;
+            }
+
+            try
+            {
+                var resultadoDeCoils = await maestroPLC.ReadCoilsAsync(1, 0, 6);
+                var resultadoDeHoldings = await maestroPLC.ReadHoldingRegistersAsync(1, 0, 8);
+                coils = resultadoDeCoils;
+                holdings = resultadoDeHoldings;
+
+            }
+            catch
+            {
+                EstadoPLC();
+
+            }
+            //try
+            //{
+            //    maestroPLC.WriteMultipleCoils(4, new bool[] { false });
+            //    maestroPLC.WriteMultipleCoils(5, new bool[] { true });
+            //    maestroPLC.WriteMultipleCoils(3, new bool[] { true });
+            //}
+            //catch
+            //{
+            //    EstadoPLC();
+            //}
+
+            //if (coils[4] == true)
+            //{
+            //    panel4.BackColor = Color.White;
+            //}
+            //else
+            //{
+            //    panel4.BackColor = Color.Red;
+            //}
+
+
+            if (coils[0] == true)
+            {
+                label1.Text = "Vehiculo Presente";
+                panel1.BackColor = Color.Green;
+
+                if (coils[4] == true){
+                  
+                    try
+                    {
+
+                        maestroPLC.WriteMultipleCoils(4, new bool[] { false });
+                    }
+                    catch
+                    {
+                        EstadoPLC();
+                    }
+
+                    using (baseEn db = new baseEn())
+                    {
+                        inicio otabla_inicio = new inicio();
+                        otabla_inicio.inicio1 = Convert.ToString(DateTime.Now);
+                        db.inicio.Add(otabla_inicio);
+                        db.SaveChanges();
+                        var xd = db.inicio.OrderByDescending(x => x.di).FirstOrDefault();
+                        var lst = db.inicio;
+                        inicioDeTransaccion = xd.inicio1; //.Max(x => x.ID_C); //Convert.ToString((from inicio in db.inicio select inicio).ToList());
+
+                    }
+
+
+                }
+
+
+                if (coils[1] == true)
+                {
+                    if (coils[5] == false)
+                    {
+                        label4.Text = "Pidiendo autorizacion...";
+                        button1.Enabled = true;
+                    }
+                    else
+                    {
+                        label4.Text = "Autorizado";
+                        panel2.BackColor = Color.Green;
+                        button1.Enabled = false;
+                    }
+
+                    if (coils[2] == true && coils[3] == true)
+                    {
+
+
+                        label5.Text = "Despachando";
+                        panel3.BackColor = Color.Green;
+                        despachando = true;
+
+                    }
+
+
+
+                }
+                else
+                {
+                    label4.Text = "No se ha pediod autorizacion";
+                    panel2.BackColor = Color.Red;
+                    maestroPLC.WriteMultipleCoils(5, new bool[] { false });
+                    button1.Enabled = false;
+                }
+            }
+            else
+            {
+
+                if (despachando != true)
+                {
+                    inicio();
+                    maestroPLC.WriteMultipleCoils(0, new bool[] { true, false, false, false });
+                    maestroPLC.WriteMultipleCoils(5, new bool[] { false });
+                }
+            }
+
+            if (despachando == true && coils[3] == false)
+            {
+
+                label5.Text = "Carga terminada";
+                panel3.BackColor = Color.Yellow;
+                button1.Enabled = false;
+
+
+                if (coils[0] == false)
+                {
+                    var litros = Convert.ToString(holdings[1]);
+                    var mililitros = Convert.ToString(holdings[2]);
+                    var litrosTotales = Convert.ToString(holdings[6]);
+                    var mililitrosTotales = Convert.ToString(holdings[7]);
+                    finDeTransaccion = Convert.ToString(DateTime.Now);
+
+
+                    using (be db = new be())
+                    {
+                        gasolina otabla_gas = new gasolina();
+                        otabla_gas.Tag = Convert.ToInt32(holdings[0]);
+                        otabla_gas.Litros = litros + "." + mililitros;
+                        otabla_gas.Densidad = (double)(int?)Convert.ToSingle(holdings[3]);
+                        otabla_gas.Temperatura = (double)(int?)Convert.ToSingle(holdings[4]);
+                        otabla_gas.Flujo = (double)(int?)Convert.ToSingle(holdings[5]);
+                        otabla_gas.Totalizado = litrosTotales + "." + mililitrosTotales;
+                        otabla_gas.Fecha_de__inicio = inicioDeTransaccion;
+                        otabla_gas.Fecha_de_terminio = finDeTransaccion;
+                        db.gasolina.Add(otabla_gas);
+                        db.SaveChanges();
+                        var lst = db.gasolina;
+                    }
+                    maestroPLC.WriteMultipleCoils(2, new bool[] { false });
+                    maestroPLC.WriteMultipleCoils(5, new bool[] { false });
+                    inicio();
+                    despachando = false;
+                    inicioDeTransaccion = null;
+                    maestroPLC.WriteMultipleCoils(4, new bool[] { true });
+
+
+                }
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //apagados++;
+            
+            //using (baseEn db = new baseEn())
+            //{
+            //    inicio otabla_inicio = new inicio();
+            //    otabla_inicio.inicio1 = inicioDeTransaccion;
+            //    otabla_inicio.apagado = apagados;
+            //    db.inicio.Add(otabla_inicio);
+            //    db.SaveChanges();
+            //    var lst = db.inicio;
+
+
+            //}
+        }
+
+        private void inicio()
+        {
+            button1.Enabled = false;
+            label1.Text = "En espera de un vehiculo";
+            label3.Text = "Estado de la conexion: ";
+            label4.Text = "No se ha pedido autorizacion";
+            label5.Text = "No despachando";
+            label6.Text = "Tag del vehiculo";
+            label7.Text = "No leida aun";
+            panel1.BackColor = Color.Red;
+            panel2.BackColor = Color.Red;
+            panel3.BackColor = Color.Red;
+            
+        }
+    }
+
+   
+
+}
